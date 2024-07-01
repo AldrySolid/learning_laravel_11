064c690b829e21e652f896c3de270b9897531e9a
Al <next2@edeni4ka.ru>
2024-07-01 15:29:35 +0300

    STEP-5 Models relations
    - Правка багов

--- app/Console/Commands/testRelations.php
+++ app/Console/Commands/testRelations.php
@@ -13,49 +13,38 @@ class testRelations extends Command
 
     protected $description = 'Test relations';
 
-    protected Category $_category;
-
-    protected Post $_post;
-
-    protected Tag $_tag;
-
-    public function __construct()
-    {
-        $this->_post     = Post::inRandomOrder()->get()->first();
-        $this->_category = Category::inRandomOrder()->get()->first();
-        $this->_tag      = Tag::inRandomOrder()->get()->first();
-
-        parent::__construct();
-    }
-
     /**
      * Execute the console command.
      * @throws \Throwable
      */
     public function handle()
     {
+        $post     = Post::inRandomOrder()->get()->first();
+        $category = Category::inRandomOrder()->get()->first();
+        $tag      = Tag::inRandomOrder()->get()->first();
+
         $result = null;
 
         switch ($this->argument('case')) {
             case 1:
                 // Пост -> Профиль -> Юзер -> почта
-                $result = $this->_post->profile->user->email;
+                $result = $post->profile->user->email;
                 break;
             case 2:
                 // Категория -> Посты
-                $result = $this->_category->posts;
+                $result = $category->posts;
                 break;
             case 3:
                 // Пост -> Теги
-                $result = $this->_post->tags;
+                $result = $post->tags;
                 break;
             case 4:
                 // Тег -> Посты
-                $result = $this->_tag->posts;
+                $result = $tag->posts;
                 break;
             case 5:
                 // Пост -> Категория -> Заголовок
-                $result = $this->_post->category->title;
+                $result = $post->category->title;
                 break;
             default:
                 $this->fail();
