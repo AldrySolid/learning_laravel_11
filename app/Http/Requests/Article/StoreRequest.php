17a44dd7f90c31c8ee4aafb28a0a87fb2c4a8816
Al <next2@edeni4ka.ru>
2025-12-09 22:55:29 +0300

    DEEPSEEK-1
    - Добавил проверку авторизации при создании статьи
    - Добавил проверку автора

--- app/Http/Requests/Article/StoreRequest.php
+++ app/Http/Requests/Article/StoreRequest.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRequest extends FormRequest
 {
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
     public function rules(): array
     {
         return [
--- app/Http/Requests/Article/UpdateRequest.php
+++ app/Http/Requests/Article/UpdateRequest.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Requests\Article;
 
+use App\Models\Article;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateRequest extends FormRequest
 {
+    public function authorize()
+    {
+        $article = Article::find($this->route('article'));
+
+        return $article && $article->profile_id == auth()->user()->profiles->first()->id;
+    }
+
     public function rules(): array
     {
         return [
-            'title'   => 'required|string',
+            'title' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('articles')->ignore($this->route('article'))
+            ],
             'content' => 'required|string',
         ];
     }
